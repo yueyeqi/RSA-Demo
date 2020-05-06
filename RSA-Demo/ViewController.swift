d95101b72683c25982b83d76bba9c2eccd5df713
岳业骑 <yueyeqi@icloud.com>
2020-05-07 00:48:00 +0800

    Demo

--- RSA-Demo/ViewController.swift
+++ RSA-Demo/ViewController.swift
@@ -12,9 +12,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //1.加载公钥
+        RSACryptor.shared()?.loadPublicKey(Bundle.main.path(forResource: "rsacert.der", ofType: nil))
+        
+        //2.加载私钥
+        RSACryptor.shared()?.loadPrivateKey(Bundle.main.path(forResource: "p.p12", ofType: nil), password: "123456")
     }
-
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let str = "hello"
+        //1.加密
+        let result = RSACryptor.shared()?.encryptData(str.data(using: .utf8))
+        print("加密的结果：\(result?.base64EncodedString())")
+        
+        //2.解密
+        let jiemi = (RSACryptor.shared()?.decryptData(result))!
+        print("解密的结果：\(String(data: jiemi, encoding: .utf8))")
+        
+    }
 
 }
 
